f1799292cc1fc8cfec8af1748df8e6a64c69b4a9
axtloss <3alouchi2006@gmail.com>
2021-07-21 17:36:23 +0200

    misunderstood previous code, works as intended now

--- src/search.rs
+++ src/search.rs
@@ -10,15 +10,25 @@ pub fn search(pkg: &str) {
     let url = format!("https://aur.archlinux.org/rpc/?v=5&type=search&arg={}",&pkg);
     
     let mut easy = Easy::new();
-    easy.url(&url).unwrap();
-    easy.write_function(|data| {
-        stdout().write_all(data).unwrap();
-        Ok(data.len())
-    }).unwrap();
-    easy.perform().unwrap();
+    easy.url("https://www.rust-lang.org/").unwrap();
 
-    let output = easy.response_code().unwrap();
-    println!("{}", output);
+    let mut data = Vec::new();
+let mut handle = Easy::new();
+handle.url(&url).unwrap();
+{
+    let mut transfer = handle.transfer();
+    transfer.write_function(|new_data| {
+        data.extend_from_slice(new_data);
+        Ok(new_data.len())
+    }).unwrap();
+    transfer.perform().unwrap();
+}
+//println!("{:?}", data);
+
+    let s = String::from_utf8_lossy(&data);
+    println!("result: {}", s);
 
     Command::new("pacman").arg("-Ss").arg(&pkg).spawn().expect("Failed to run pacman");
+
+    //let parsed = json::parse(&output)
 }  
