afb70cbe3b14f88e0eea63a35ce9807e96601285
jnats <jnats@salyut.one>
2021-09-25 02:13:23 +0100

    rem -> rm

--- src/mods/help.rs
+++ src/mods/help.rs
@@ -4,7 +4,7 @@ pub fn help() { // work on a proper error message, the python ame one isnt reall
     inf(format!("Usage:"));
     println!("
 ame -S   / -Sy / ins <pkg> - install a package
-ame -R   / -Rs / rem <pkg> - remove  a package
+ame -R   / -Rs / rm  <pkg> - remove  a package
 ame -Syu / upg             - upgrade all packages to latest version
 ame -Ss  / sea       <pkg> - search for a package
 ame -Sa  / aursea    <pkg> - search for a package in the aur
