dcc630fc681f02355c4a6e36ec890c4d48d485a4
jnats <jnats@salyut.one>
2021-09-28 10:27:33 +0100

    fixed search and added secret feature

--- src/mods/ver.rs
+++ src/mods/ver.rs
@@ -3,7 +3,7 @@ use ansi_term::Colour;
 
 pub fn ver() {
     println!("");
-    inf(format!("ame - v2.1.1"));
+    inf(format!("ame - v2.1.2"));
     println!("");
     inf(format!("Contributors:"));
     println!("- axtlos <axtlos@salyut.one>");
