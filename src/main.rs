e9dac80a51688f57244bf61a761c42a5670a895d
jnats <jnats@salyut.one>
2021-10-02 21:39:03 +0100

    removed old comments

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,9 @@ use mods::{
 use std::{env, process::exit, process::Command};
 
 fn main() {
-    // let statements
     let args: Vec<String> = env::args().collect();
     let mut pkgs: Vec<String> = env::args().skip(2).collect();
 
-    // args catch
     if args.len() <= 1 {
         help();
         exit(1);
@@ -33,48 +31,37 @@ fn main() {
 
     argssort(&mut pkgs);
 
-    // install
     if oper == "-S" || oper == "-Sn" || oper == "ins" {
         inssort(noconfirm, pkgs);
 
-    // remove
     } else if oper == "-R" || oper == "-Rn " || oper == "-Rsn" || oper == "-Rs" || oper == "rm" {
         uninstall(noconfirm, pkgs);
 
-    // upgrade
     } else if oper == "-Syu" || oper == "-Syun" || oper == "upg" {
         upgrade(noconfirm);
 
-    // update
     } else if oper == "-Sy" || oper == "upd" {
         update();
 
-    // general search
     } else if oper == "-Ss" || oper == "sea" {
         r_search(&args[2]);
         a_search(&args[2]);
 
-    // aur search
     } else if oper == "-Sa" || oper == "aursea" {
         a_search(&args[2]);
 
-    // repo search
     } else if oper == "-Sr" || oper == "repsea" {
         r_search(&args[2]);
 
-    // clear cache !! DEBUG ONLY !! DO NOT DO THIS IF YOU DONT KNOW WHAT YOURE DOING !!
     } else if oper == "-Cc" || oper == "clr" {
         clearcache();
 
-    // version / contrib
     } else if oper == "-v" || oper == "-V" || oper == "ver" {
         ver();
 
-    // help
     } else if oper == "-h" || oper == "help" {
         help();
 
-    // pacman passthrough
     } else {
         let pass = Command::new("pacman")
             .args(env::args().skip(1))
