104c76efbe070b07d61137a2fbf5ebc805799f83
jnats <jnats@salyut.one>
2021-10-04 20:22:30 +0100

    horrid, awful fix to #4

--- src/mods/inssort.rs
+++ src/mods/inssort.rs
@@ -4,21 +4,41 @@ use std::process::{Command, Stdio};
 pub fn inssort(noconfirm: bool, pkgs: Vec<String>) {
     let mut repo = vec![];
     let mut aur = vec![];
+    let re = regex::Regex::new(r"(\S+)((?:>=|<=)\S+$)").unwrap();
+    let reg = regex::Regex::new(r"((?:>=|<=)\S+$)").unwrap();
     for pkg in pkgs {
-        let out = Command::new("pacman")
-            .arg("-Ss")
-            .arg(format!("^{}$", &pkg))
-            .stdout(Stdio::null())
-            .status()
-            .expect("Something has gone wrong.");
-        match out.code() {
-            Some(0) => repo.push(pkg),
-            Some(1) => aur.push(pkg),
-            Some(_) => err_unrec(format!("Something has gone terribly wrong")),
-            None => err_unrec(format!("Process terminated")),
+        let caps = re.captures(&pkg);
+        match caps {
+            Some(_) => {
+                let out = Command::new("pacman")
+                    .arg("-Ss")
+                    .arg(format!("^{}$", caps.unwrap().get(1).map_or("", |m| m.as_str())))
+                    .stdout(Stdio::null())
+                    .status()
+                    .expect("Something has gone wrong.");
+                match out.code() {
+                    Some(0) => repo.push(reg.replace_all(&pkg, "").to_string()),
+                    Some(1) => aur.push(pkg),
+                    Some(_) => err_unrec(format!("Something has gone terribly wrong")),
+                    None => err_unrec(format!("Process terminated")),
+                }
+            }
+            None => {
+                let out = Command::new("pacman")
+                    .arg("-Ss")
+                    .arg(format!("^{}$", &pkg))
+                    .stdout(Stdio::null())
+                    .status()
+                    .expect("Something has gone wrong.");
+                match out.code() {
+                    Some(0) => repo.push(pkg),
+                    Some(1) => aur.push(pkg),
+                    Some(_) => err_unrec(format!("Something has gone terribly wrong")),
+                    None => err_unrec(format!("Process terminated")),
+                }
+            }
         }
     }
-
     if repo.len() != 0 {
         sec(format!("Installing repo packages: {}", &repo.join(", ")));
         install(noconfirm, &repo.join(" "));
