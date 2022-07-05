7baaced9056b9034aa9c548c143a4fd333ea385a
trivernis <trivernis@protonmail.com>
2022-07-05 20:51:14 +0200

    Fix the one and only clippy warning
    Signed-off-by: trivernis <trivernis@protonmail.com>

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -13,6 +13,5 @@ use rusqlite::Connection;
 
 fn get_database_connection() -> Connection {
     let db_path = format!("{}/.local/share/ame/db.sqlite", env::var("HOME").unwrap());
-    let conn = Connection::open(PathBuf::from(db_path)).expect("Couldn't connect to database");
-    conn
+    Connection::open(PathBuf::from(db_path)).expect("Couldn't connect to database")
 }
