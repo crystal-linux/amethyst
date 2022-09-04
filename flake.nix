3093f4241ece34bc960c03c8fdc35b2fd058f211
Michal S <michal@tar.black>
2022-09-04 17:03:01 +0100

    Simplify and update Nix flake

--- flake.nix
+++ flake.nix
@@ -6,10 +6,6 @@
       url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
@@ -17,49 +13,16 @@
     nixpkgs,
     utils,
     naersk,
-    fenix,
+    ...
   }:
     utils.lib.eachDefaultSystem (system: let
         pkgs = nixpkgs.legacyPackages."${system}";
-        toolchain = with fenix.packages."${system}";
-          combine [
-            minimal.rustc
-            minimal.cargo
-            targets.x86_64-pc-windows-gnu.latest.rust-std
-            targets.x86_64-unknown-linux-gnu.latest.rust-std
-          ];
-        naersk-lib = naersk.lib."${system}".override {
-          cargo = toolchain;
-          rustc = toolchain;
-        };
+        naersk-lib = naersk.lib."${system}";
       in rec
       {
         packages.amethyst = naersk-lib.buildPackage {
           pname = "Amethyst";
           root = ./.;
-        };
-
-        packages.amethyst-win = naersk-lib.buildPackage {
-          pname = "Amethyst";
-          root = ./.;
-          strictDeps = true;
-          depsBuildBuild = with pkgs; [
-            pkgsCross.mingwW64.stdenv.cc
-            pkgsCross.mingwW64.windows.pthreads
-          ];
-          nativeBuildInputs = with pkgs; [
-            (
-              if system == "x86_64-linux"
-              then wineWowPackages.stable
-              else hello
-            )
-          ];
-          CARGO_BUILD_TARGET = "x86_64-pc-windows-gnu";
-          CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUNNER = pkgs.writeScript "wine-wrapper" ''
-            export WINEPREFIX="$(mktemp -d)"
-            exec wine64 $@
-          '';
-          doCheck = true;
         };
 
         packages.default = packages.amethyst;
@@ -77,11 +40,8 @@
             cargo-audit
             rustfmt
             clippy
-
-            openssl
-            sqlite
-            pkg-config
           ];
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
         };
 
         formatter = pkgs.alejandra;
