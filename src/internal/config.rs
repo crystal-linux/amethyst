77d914c01d93f0d9a0a71e835943602844a3759f
Fries <fries1234@protonmail.com>
2022-09-08 08:12:02 -0700

    config: use config crate
    Parsing of the config is now done with the config crate

--- src/internal/config.rs
+++ src/internal/config.rs
@@ -1,58 +1,89 @@
 #![allow(clippy::module_name_repetitions)]
 
-use serde::Deserialize;
-use std::{env, fs, path::PathBuf};
+use config::FileFormat;
+use lazy_static::lazy_static;
+use serde::{Deserialize, Serialize};
+use std::fs;
 
-#[derive(Debug, Deserialize)]
+use super::utils::get_config_dir;
+
+#[derive(Default, Debug, Deserialize, Serialize)]
 pub struct Config {
     pub base: ConfigBase,
     pub extra: ConfigExtra,
     pub bin: ConfigBin,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct ConfigBase {
     pub pacdiff_warn: bool,
     pub highlight_optdepends: bool,
     pub powerpill: bool,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct ConfigExtra {
     pub uwu: Option<bool>,
     pub uwu_debug: Option<bool>,
     pub review_user_shell: bool,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct ConfigBin {
     pub sudo: Option<String>,
 }
 
-impl Default for Config {
+impl Default for ConfigBase {
     fn default() -> Self {
         Self {
-            base: ConfigBase {
-                pacdiff_warn: true,
-                highlight_optdepends: true,
-                powerpill: false,
-            },
-            extra: ConfigExtra {
-                uwu: None,
-                uwu_debug: None,
-                review_user_shell: false,
-            },
-            bin: ConfigBin {
-                sudo: Some("sudo".to_string()),
-            },
+            pacdiff_warn: true,
+            highlight_optdepends: true,
+            powerpill: false,
         }
     }
 }
 
-pub fn read() -> Config {
-    let config_path = PathBuf::from(env::var("HOME").unwrap()).join(".config/ame/config.toml");
-    match fs::read_to_string(config_path) {
-        Ok(contents) => toml::from_str(&contents).expect("Could not parse the config file"),
-        Err(_) => Config::default(),
+impl Default for ConfigExtra {
+    fn default() -> Self {
+        Self {
+            uwu: None,
+            uwu_debug: None,
+            review_user_shell: false,
+        }
+    }
+}
+
+impl Default for ConfigBin {
+    fn default() -> Self {
+        Self {
+            sudo: Some("sudo".to_string()),
+        }
+    }
+}
+
+impl Config {
+    pub fn read() -> Self {
+        let config_path = get_config_dir().join("config.toml");
+        if config_path.exists() {
+            let builder = config::Config::builder()
+                .add_source(config::File::from_str(
+                    &toml::ser::to_string(&Config::default()).unwrap(),
+                    FileFormat::Toml,
+                ))
+                .add_source(config::File::with_name(config_path.to_str().unwrap()));
+            let conf = builder.build().unwrap();
+            conf.try_deserialize().unwrap()
+        } else {
+            let default_conf = Config::default();
+            let toml_string = toml::ser::to_string_pretty(&default_conf).unwrap();
+            fs::write(config_path, toml_string).unwrap();
+            default_conf
+        }
+    }
+    pub fn get() -> &'static Config {
+        lazy_static! {
+            static ref CONFIG: Config = Config::read();
+        }
+        &*CONFIG
     }
 }
