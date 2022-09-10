ef18fa9ce5f9abf86ef9d05f015cfddab87ebea9
Michal S <michal@tar.black>
2022-09-10 14:30:29 +0100

    Add codename to version output

--- src/args.rs
+++ src/args.rs
@@ -3,8 +3,10 @@
 use crate::operations::SearchBy;
 use clap::{Parser, Subcommand, ValueHint};
 
+static VERSION: &str = concat!(env!("CARGO_PKG_VERSION"), " (Fish)");
+
 #[derive(Debug, Clone, Parser)]
-#[clap(bin_name = "ame", name = "Amethyst", version = env ! ("CARGO_PKG_VERSION"), about = env ! ("CARGO_PKG_DESCRIPTION"), infer_subcommands = true)]
+#[clap(bin_name = "ame", name = "Amethyst", version = VERSION, about = env ! ("CARGO_PKG_DESCRIPTION"), infer_subcommands = true)]
 pub struct Args {
     #[clap(subcommand)]
     pub subcommand: Option<Operation>,
