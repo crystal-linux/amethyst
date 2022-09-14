b3406d5d4b69f8a1e20a18071136d71e3f23fd1e
Fries <fries1234@protonmail.com>
2022-09-13 21:59:33 -0700

    builder/paccache: more obvious func/var names

--- src/builder/paccache.rs
+++ src/builder/paccache.rs
@@ -3,25 +3,25 @@ use crate::internal::{commands::ShellCommand, error::AppResult};
 #[derive(Debug, Default)]
 pub struct PaccacheBuilder {
     keep: i32,
-    keep_ins: bool,
-    quiet: bool,
+    keep_ins_pkgs: bool,
+    quiet_output: bool,
 }
 
 impl PaccacheBuilder {
-    pub fn keep(mut self, keep: i32) -> Self {
+    pub fn set_keep(mut self, keep: i32) -> Self {
         self.keep = keep;
 
         self
     }
 
-    pub fn keep_ins(mut self, keep_ins: bool) -> Self {
-        self.keep_ins = keep_ins;
+    pub fn keep_ins_pkgs(mut self, keep_ins_pkgs: bool) -> Self {
+        self.keep_ins_pkgs = keep_ins_pkgs;
 
         self
     }
 
-    pub fn quiet(mut self, quiet: bool) -> Self {
-        self.quiet = quiet;
+    pub fn quiet_output(mut self, quiet_output: bool) -> Self {
+        self.quiet_output = quiet_output;
 
         self
     }
@@ -30,11 +30,11 @@ impl PaccacheBuilder {
     pub async fn remove(self) -> AppResult<()> {
         let mut command = ShellCommand::paccache().elevated();
 
-        if self.quiet {
+        if self.quiet_output {
             command = command.arg("-q");
         }
 
-        if self.keep_ins {
+        if self.keep_ins_pkgs {
             command = command.arg("-u")
         }
 
